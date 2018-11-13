20bac9d176cd4035c00154ee80018f14b42c2281
Hubert Figuière <hub@figuiere.net>
2018-11-13 15:13:26 -0800

    background_script: use absolute URL for the script injection

--- extension/pixelbug.js
+++ extension/pixelbug.js
@@ -5,18 +5,18 @@ let state = {
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   if (state.loaded[tab.id] && state.injected[tab.id]) {
-    chrome.tabs.executeScript(tab.id, { file: 'toolbar/eject.js' })
+    chrome.tabs.executeScript(tab.id, { file: '/toolbar/eject.js' })
     state.injected[tab.id] = false
   }
   else if (state.loaded[tab.id] && !state.injected[tab.id]) {
-    chrome.tabs.executeScript(tab.id, { file: 'toolbar/inject.js' })
+    chrome.tabs.executeScript(tab.id, { file: '/toolbar/inject.js' })
     state.injected[tab.id] = true
   }
   else {
-    chrome.tabs.insertCSS(tab.id,     { file: 'toolbar/bundle.css' })
-    chrome.tabs.executeScript(tab.id, { file: 'web-components.polyfill.js' })
-    chrome.tabs.executeScript(tab.id, { file: 'toolbar/bundle.js' })
-    chrome.tabs.executeScript(tab.id, { file: 'toolbar/inject.js' })
+    chrome.tabs.insertCSS(tab.id,     { file: '/toolbar/bundle.css' })
+    chrome.tabs.executeScript(tab.id, { file: '/web-components.polyfill.js' })
+    chrome.tabs.executeScript(tab.id, { file: '/toolbar/bundle.js' })
+    chrome.tabs.executeScript(tab.id, { file: '/toolbar/inject.js' })
 
     state.loaded[tab.id]    = true
     state.injected[tab.id]  = true
